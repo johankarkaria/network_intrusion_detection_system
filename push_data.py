d14728aa3e3e6f8958133b71d610c0c442d3cc8b
Johan Karkaria <johankar555@gmail.com>
2026-06-01 18:00:51 +0530

    changes push_data.py

--- push_data.py
+++ push_data.py
@@ -1,79 +1,66 @@
 
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
+import os
+import sys
+import json
 
-uri = "mongodb+srv://jamesir637276_db_user:johankar4545@cluster0.3crc5hy.mongodb.net/?appName=Cluster0"
+from dotenv import load_dotenv
+load_dotenv()
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+import certifi
+ca=certifi.where()
 
+import pandas as pd
+import numpy as np
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
 
-# import os
-# import sys
-# import json
-
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# MONGO_DB_URL=os.getenv("MONGO_DB_URL")
-# print(MONGO_DB_URL)
-
-# import certifi
-# ca=certifi.where()
-
-# import pandas as pd
-# import numpy as np
-# import pymongo
-# from networksecurity.exception.exception import NetworkSecurityException
-# from networksecurity.logging.logger import logging
-
-# class NetworkDataExtract():
-#     def __init__(self):
-#         try:
-#             pass
-#         except Exception as e:
-#             raise NetworkSecurityException(e,sys)
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
         
-#     def csv_to_json_convertor(self,file_path):
-#         try:
-#             data=pd.read_csv(file_path)
-#             data.reset_index(drop=True,inplace=True)
-#             records=list(json.loads(data.T.to_json()).values())
-#             return records
-#         except Exception as e:
-#             raise NetworkSecurityException(e,sys)
+    def csv_to_json_convertor(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
         
-#     def insert_data_mongodb(self,records,database,collection):
-#         try:
-#             self.database=database
-#             self.collection=collection
-#             self.records=records
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
 
-#             self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
-#             self.database = self.mongo_client[self.database]
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
             
-#             self.collection=self.database[self.collection]
-#             self.collection.insert_many(self.records)
-#             return(len(self.records))
-#         except Exception as e:
-#             raise NetworkSecurityException(e,sys)
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return(len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
         
-# if __name__=='__main__':
-#     FILE_PATH="Network_Data\phisingData.csv"
-#     DATABASE="KRISHAI"
-#     Collection="NetworkData"
-#     networkobj=NetworkDataExtract()
-#     records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
-#     print(records)
-#     no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
-#     print(no_of_records)
+if __name__=='__main__':
+    FILE_PATH="Network_Data\phisingData.csv"
+    DATABASE="KRISHAI"
+    Collection="NetworkData"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
+    print(no_of_records)
         
+
+
+
 
 
