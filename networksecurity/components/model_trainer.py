59763c236600fab28e6178f98cf8bf37c988bc60
Johan Karkaria <johankar555@gmail.com>
2026-06-02 17:55:19 +0530

    added files

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -31,7 +31,7 @@ from urllib.parse import urlparse
 import dagshub
 dagshub.init(repo_owner='johankar555', repo_name='network_intrusion_detection_system', mlflow=True)
 
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/johankar555/networksecurity.mlflow"
 os.environ["MLFLOW_TRACKING_USERNAME"]="johankar555"
 os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
 
@@ -48,7 +48,7 @@ class ModelTrainer:
             raise NetworkSecurityException(e,sys)
         
     def track_mlflow(self,best_model,classificationmetric):
-        mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+        mlflow.set_registry_uri("https://dagshub.com/johanka4555/networksecurity.mlflow")
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
