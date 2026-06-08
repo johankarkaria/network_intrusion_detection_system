f97d2acc9f97c09354dacb3ce920881e92dfae7d
Johan Karkaria <johankar555@gmail.com>
2026-06-08 17:17:49 +0530

    changes file

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -28,8 +28,8 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 load_dotenv()
-# import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+
+
 os.environ["DAGSHUB_USER_TOKEN"] = os.getenv('DAGSHUB_TOKEN')
 
 import dagshub
